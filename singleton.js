ba8c07695f2f4042b7caf766c974d98bdba28bf9
unknown <arthur.picerna@A19-DAP-312.aviatur.com.co>
2016-07-06 16:17:37 -0500

    first commit

---
+++ singleton.js
@@ -0,0 +1,16 @@
+﻿/*** 
+ * A singleton pattern is typically used to instanciate a given object only once
+ * and eventually avoid setting global variables as checkers for that instanciation
+ */
+var Singleton = function() {
+    if(Singleton.prototype._singletonInstance) {
+        return Singleton.prototype._singletonInstance;
+    }
+    HotelRefreshInterval.prototype._singletonInstance = this; // or 'true' if you don't actually need to retrieve the object
+    
+    // do whatever
+};
+
+var a = new Singleton(),
+    b = Singleton();
+console.log(a === b); // prints 'true'
