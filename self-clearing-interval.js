374b591a2fe80c2fb2c64ea0955545dbb8b136d6
unknown <arthur.picerna@A19-DAP-312.aviatur.com.co>
2016-07-06 16:45:04 -0500

    self clearing array

---
+++ self-clearing-interval.js
@@ -0,0 +1,11 @@
+﻿/***
+ * A self-clearing interval to avoid infinite repetition
+ */
+var count = 0,
+    limit = 6,
+    time = 600000, // in milliseconds
+    myInterval = setInterval(function () {
+        count++;
+	// do whatever
+        if(count === limit) clearInterval(myInterval);
+    }, time);
