8718fee8219bbfa556cc8a05c3ccd2d799b00489
decksterr <arthurpicerna@gmail.com>
2016-08-05 10:11:44 -0500

    Update singleton.js

--- singleton.js
+++ singleton.js
@@ -13,17 +13,19 @@ var Singleton = function() {
     console.log('Singleton executed');
 };
 
-// --- Case 'true' ---
-// Note: in case you dont actually need to set the object but rather run the code inside it only once:
-//       -> set 'true' as _singletonInstance
-//       -> the non-use of the 'new' keyword is because we are just accessing the function's prototype property
+/* --- Case 'true' ---
+ * Note: in case you dont actually need to set the object but rather run the code inside it only once:
+ *      -> set 'true' as _singletonInstance
+ *      -> the non-use of the 'new' keyword is because we are just accessing the function's prototype property
+ */
 Singleton();
 Singleton(); // prints 'Singleton executed' only once
 
-// --- Case 'this' ---
-// Note: setting 'this' as _singletonInstance will store many times same instance of same object (Droste effect style)
-//       -> if you just need a globally accessible object refer to /avoid-globals.js
-//       -> use the 'new' keyword upon declaration if using 'this'
+/* --- Case 'this' ---
+ * Note: setting 'this' as _singletonInstance will store many times same instance of same object (Droste effect style)
+ *      -> if you just need a globally accessible object refer to /avoid-globals.js
+ *      -> use the 'new' keyword upon declaration if using 'this'
+ */
 var a = new Singleton(),
     b = new Singleton();
 console.log(a === b); // prints 'true'
