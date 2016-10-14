9b8c5e7991721ba359987dc57958b443de3f04de
decksterr <arthurpicerna@gmail.com>
2016-10-14 15:50:28 -0500

    encapsulated public object pattern
    and some adjustments

--- avoid-globals.js
+++ avoid-globals.js
@@ -8,25 +8,54 @@ var myNameSpace = function() {
     function init() {
         return this.current = 1;
     }
-    function change() {
-        return this.current++; // note: don't increment while returning if you actually need to return the value
+    function increment() {
+        this.current++;
+    }
+    function getValue() {
+        return this.current;
     }
     // return a pointers object to make those variables available outside the scope
     return {
         current: current,
         foo: foo,
         init: init,
-        set: change // public alias for actual method
+        change: increment,  // public alias for actual method
+        getValue: getValue
     };
 }(); // self-execute
-console.log(myNameSpace); // prints the returned object
-myNameSpace.set(); // invokes the change() method
+console.log(myNameSpace);   // prints the global object
+myNameSpace.init();
+myNameSpace.change();       // invokes the increment() method
+myNameSpace.getValue();     // prints 2
 
 /*
  * An encapsulated self-executing function, when you need things to be tidy (but don't actually need to see what's underneath)
  */
 (function() { // wrap the function in between parenthesis to keep it from being globally exposed
+    
     var ba = 'ba';
     console.log('Independance is key');
-})(); // the last () is for self-execution
+    
+})(); // self-execute
 console.log(ba); // throws an undefined exception
+
+/*
+ * An encapsulated object, that may be called anytime needed
+ */
+(function(public) {
+	
+    // private variables
+	var foo = {"bar":"baz"};
+    
+    // public method that initiates private methods callbacks
+	public.someFunction = function(param) {		
+		privateFunction(param, foo);				
+	};
+
+	function privateFunction(param, foo) {
+        console.log(param);
+        console.log(foo.bar === 'baz');
+	}
+
+})(window.some_namespace = window.some_namespace || {});
+window.some_namespace.someFunction('here be your output:');
