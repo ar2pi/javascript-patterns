2c58d6b40ba20ca5586c4b76def44c43a32a13b8
decksterr <arthurpicerna@gmail.com>
2016-10-19 16:22:39 -0500

    async image loading

---
+++ async-image.js
@@ -0,0 +1,24 @@
+﻿
+/*
+ * load image in browser cache
+ */
+(new Image()).src = 'url/to/image.svg';
+// Note: src property can also be set multiple times to load multiple images via same object
+
+/*
+ * defer rendering
+ */
+var lazyImage = new Image();
+lazyImage.onload = function() {
+    target.src = this.src;
+};
+lazyImage.src = 'url/to/image.svg';
+
+/*
+ * with jquery
+ */
+jQuery.get(imgSrc)
+    // prevent background from being changed if no img is found
+    .done(function() {
+        jQuery('.target').css('background-image', 'url(' + imgSrc + ')');
+    });
