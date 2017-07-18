0c4f442a4f54223491dbdb91dbbb3cec19ed71f6
decksterr <arthurpicerna@gmail.com>
2017-07-18 10:41:21 -0500

    Update getFullDate.js

--- getFullDate.js
+++ getFullDate.js
@@ -1,13 +1,13 @@
 /**
- * Safe Date parser for ECMA-262 spec
- * input format:
- *      (2017([-|/]02)?([-|/]24)?)?([T| ]23(:59)?(:59)?(Z)?)?
+ * @description Safe Date parser for ECMA-262 spec
+ * @param {string} input yyyy[-|/]mm[-|/]dd[T| ]hh:mm:ssZ every unit is ommitable except the year
+ * @returns {Date}
  */
 function getFullDate(input) {
     input = (input) ? input.trim() : '';
     if (input.length) {
         var output,
-            f = [], d = [], t = [],
+            f = [], d = [], t = [], tz = [],
             fs, ds;
         fs = input.match(/T| /);
         (fs !== null) ? f = input.split(fs[0]) : f[0] = input;
@@ -18,11 +18,12 @@ function getFullDate(input) {
                 d[i] = '1';
         output = new Date(d[0], parseInt(d[1]) - 1, d[2]);
         if (typeof f[1] !== 'undefined') {
-            t = f[1].split(':');
+            tz = f[1].split('Z');
+            t = tz[0].split(':');
             if (t.length !== 3)
                 for (var i = 2; i > 0, typeof t[i] === 'undefined'; i--)
                     t[i] = '0';
-            output.setHours(t[0], t[1], t[2].split('Z')[0]);
+            (tz.length > 1) ? output.setUTCHours(t[0], t[1], t[2]) : output.setHours(t[0], t[1], t[2]);
         }
     } else {
         output = new Date();
